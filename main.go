d386c98182b36eeb4a1cdd9940c3708c0b52538b
AkashiSN <btorntireinvynriy@gmail.com>
2018-10-12 15:48:51 +0900

    gameの処理を分けた

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/badminton"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/basketball"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/dodge"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/j_dodge"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/j_volleyball"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/soccer"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/softball"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/tabletennis"
-	"github.com/AkashiSN/Sports-Festa-Live/model/game/tennis"
+	"github.com/AkashiSN/Sports-Festa-Live/model/game"
 	"github.com/AkashiSN/Sports-Festa-Live/model/team"
 )
 
 func main() {
 	team := team.LoadTeam()
-	badminton.Manage(team.Badminton)
-	basketball.Manage(team.Basketball)
-	dodge.Manage(team.Dodge)
-	j_dodge.Manage(team.J_dodge)
-	j_volleyball.Manage(team.J_volleyball)
-	soccer.Manage(team.Soccer)
-	softball.Manage(team.Softball)
-	tabletennis.Manage(team.Tabletennis)
-	tennis.Manage(team.Tennis)
+	game.GameStart(team)
 }
