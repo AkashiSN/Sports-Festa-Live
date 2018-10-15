c5bb43eb77572841bd4beef2076c00c9ec713b34
AkashiSN <btorntireinvynriy@gmail.com>
2018-10-15 23:42:15 +0900

    試合番号割当に完全に勝利した

--- model/match/graph.go
+++ model/match/graph.go
@@ -7,10 +7,10 @@ import (
 // InitGraph 正規グラフを作成する
 func InitGraph(teams []string) [][]Match {
 	// 正規グラフを作成する
-	LayerCount := int(math.Ceil(math.Log2(float64(len(teams)))))
+	LayerCount := int(math.Ceil(math.Log2(float64(len(teams))))) + 1
 	graph := make([][]Match, LayerCount)
 	for i := 0; i < LayerCount; i++ {
-		graph[i] = make([]Match, int(math.Pow(2, float64(i+1))))
+		graph[i] = make([]Match, int(math.Pow(2, float64(i))))
 	}
 
 	// インデックスを振る
@@ -23,19 +23,17 @@ func InitGraph(teams []string) [][]Match {
 	}
 
 	// 子の座標の計算を行う
-	for i := 0; i < LayerCount; i++ {
+	for i := 0; i < LayerCount-1; i++ {
 		for j := 0; j < int(math.Pow(2, float64(i))); j++ {
-			if i != LayerCount-1 {
-				graph[i][j].TeamA.ChildX = 2*(j+1) - 2
-				graph[i][j].TeamA.ChildY = i + 1
-				graph[i][j].TeamB.ChildX = 2*(j+1) - 1
-				graph[i][j].TeamB.ChildY = i + 1
-			}
+			graph[i][j].TeamA.ChildX = 2*(j+1) - 2
+			graph[i][j].TeamA.ChildY = i + 1
+			graph[i][j].TeamB.ChildX = 2*(j+1) - 1
+			graph[i][j].TeamB.ChildY = i + 1
 		}
 	}
 
 	// チームを割り当てる（シードかどうかも）
-	for j := 0; j < len(graph[LayerCount-1])-1; j++ {
+	for j := 0; j < len(graph[LayerCount-1]); j++ {
 		if teams[j] == "" {
 			graph[LayerCount-1][j].IsSeed = true
 		}
@@ -43,34 +41,35 @@ func InitGraph(teams []string) [][]Match {
 	}
 
 	// 試合番号を振る
-	matchCount := 0
+	matchCount := 1
 	// トーナメント表の第1層について
-	for i := 0; i < int(math.Pow(2, float64(LayerCount))); i += 4 {
+	for i := 0; i < int(math.Pow(2, float64(LayerCount-1)))-3; i += 4 {
 		for j := i; j < i+3; j++ {
 			if teams[j] == "" && teams[j+1] == "" {
-				graph[LayerCount-1][i].MatchNum = matchCount
+				graph[LayerCount-2][j/2].MatchNum = matchCount
+				graph[LayerCount-2][j/2+1].MatchNum = matchCount
 				matchCount++
 			}
 			if j != i+1 && teams[j] != "" && teams[j+1] != "" {
-				graph[LayerCount-1][i].MatchNum = matchCount
+				graph[LayerCount-2][j/2].MatchNum = matchCount
 				matchCount++
 			}
 		}
 	}
 	// トーナメント表の第2層について
 	count := 0
-	for i := 0; i < int(math.Pow(2, float64(LayerCount))); i += 4 {
+	for i := 0; i < int(math.Pow(2, float64(LayerCount-1)))-3; i += 4 {
 		if (teams[i+1] == "") != (teams[i+2] == "") {
 			if teams[i+1] == "" {
-				graph[LayerCount-2][count].MatchNum = matchCount
+				graph[LayerCount-3][count].MatchNum = matchCount
 				matchCount++
 			}
 			if teams[i+2] == "" {
-				graph[LayerCount-2][count].MatchNum = matchCount
+				graph[LayerCount-3][count].MatchNum = matchCount
 				matchCount++
 			}
 		} else if !(teams[i+1] == "" && teams[i+2] == "") {
-			graph[LayerCount-2][count].MatchNum = matchCount
+			graph[LayerCount-3][count].MatchNum = matchCount
 			matchCount++
 		}
 		count++
@@ -81,6 +80,39 @@ func InitGraph(teams []string) [][]Match {
 			graph[i][j].MatchNum = matchCount
 			matchCount++
 		}
+	}
+
+	// トーナメント表を更新する
+	for i := 0; i < len(graph)-1; i++ {
+		for j := 0; j < len(graph[i]); j++ {
+			xa := graph[i][j].TeamA.ChildX
+			ya := graph[i][j].TeamA.ChildY
+			xb := graph[i][j].TeamB.ChildX
+			yb := graph[i][j].TeamB.ChildY
+			if graph[ya][xa].IsSeed {
+				graph[i][j].Winner = graph[yb][xb].Winner
+			} else if graph[yb][xb].IsSeed {
+				graph[i][j].Winner = graph[ya][xa].Winner
+			} else {
+				graph[i][j].TeamA.Name = graph[ya][xa].Winner
+				graph[i][j].TeamB.Name = graph[yb][xb].Winner
+			}
+		}
+	}
+
+	// トーナメント表の第2層について更新する
+	for i := 0; i < int(math.Pow(2, float64(LayerCount-3))); i++ {
+		xa := graph[LayerCount-3][i].TeamA.ChildX
+		ya := graph[LayerCount-3][i].TeamA.ChildY
+		xb := graph[LayerCount-3][i].TeamB.ChildX
+		yb := graph[LayerCount-3][i].TeamB.ChildY
+		if graph[ya][xa].Winner != "" && graph[yb][xb].Winner != "" {
+			graph[LayerCount-2][i*2].TeamA.Name = graph[ya][xa].Winner
+			graph[LayerCount-2][i*2].TeamB.Name = graph[yb][xb].Winner
+			graph[LayerCount-2][i*2+1].TeamA.Name = graph[ya][xa].Winner
+			graph[LayerCount-2][i*2+1].TeamB.Name = graph[yb][xb].Winner
+		}
+
 	}
 
 	return graph
@@ -94,7 +126,19 @@ func UpdateGraph(graph [][]Match) [][]Match {
 			ya := graph[i][j].TeamA.ChildY
 			xb := graph[i][j].TeamB.ChildX
 			yb := graph[i][j].TeamB.ChildY
-			if graph[yb][xb].IsSeed {
+			if graph[ya][xa].MatchNum == 0 {
+				xa1 := graph[ya][xa].TeamA.ChildX
+				ya1 := graph[ya][xa].TeamA.ChildY
+				xa, ya = xa1, ya1
+			}
+			if graph[yb][xb].MatchNum == 0 {
+				xb1 := graph[yb][xb].TeamB.ChildX
+				yb1 := graph[yb][xb].TeamB.ChildY
+				xb, yb = xb1, yb1
+			}
+			if graph[ya][xa].IsSeed {
+				graph[i][j].Winner = graph[yb][xb].Winner
+			} else if graph[yb][xb].IsSeed {
 				graph[i][j].Winner = graph[ya][xa].Winner
 			} else {
 				graph[i][j].TeamA.Name = graph[ya][xa].Winner
@@ -102,5 +146,6 @@ func UpdateGraph(graph [][]Match) [][]Match {
 			}
 		}
 	}
+
 	return graph
 }
--- model/match/init.go
+++ model/match/init.go
@@ -2,6 +2,7 @@ package match
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
@@ -17,4 +18,18 @@ func LoadTime() Times {
 		log.Fatal(err)
 	}
 	return time
+}
+
+// DebugDump デバックように出力する
+func DebugDump(graph [][]Match) {
+	for i := 0; i < len(graph); i++ {
+		for j := 0; j < len(graph[i]); j++ {
+			fmt.Printf("\nmatchNum:%d\n", graph[i][j].MatchNum)
+			fmt.Printf("parent(x,y):(%d,%d)\n", j, i)
+			fmt.Printf("childA(x,y):(%d,%d)\n", graph[i][j].TeamA.ChildX, graph[i][j].TeamA.ChildY)
+			fmt.Printf("childB(x,y):(%d,%d)\n", graph[i][j].TeamB.ChildX, graph[i][j].TeamB.ChildY)
+			fmt.Printf("%s VS %s\n", graph[i][j].TeamA.Name, graph[i][j].TeamB.Name)
+			fmt.Printf("Winner:%s\n", graph[i][j].Winner)
+		}
+	}
 }
